b47629ebb71d8c12e515265f93c28b5e5c1d6d6b
Sinistar02 <101465623+Sinistar02@users.noreply.github.com>
2023-10-11 19:23:54 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n,m,i,j,t,ii,jj,k;
+    scanf("%d %d",&n,&m);
+    int nlist[n];
+    for(k=1;k<=n;k++){
+        nlist[k-1]=k;
+    }
+    for(k=0;k<m;k++){
+        scanf("%d %d",&i,&j);
+        i--;
+        j--;
+        ii=nlist[j];
+        jj=nlist[i];
+        nlist[i]=ii;
+        nlist[j]=jj;
+    }
+    for(int k=0;k<n;k++){
+        printf("%d ",nlist[k]);
+    }
+    return 0;
+}
