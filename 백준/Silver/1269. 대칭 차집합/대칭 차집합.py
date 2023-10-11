097db9dc618fa8a176fa901b5d53bceac8d8774d
Sinistar02 <101465623+Sinistar02@users.noreply.github.com>
2023-10-11 19:37:42 +0900

    [Silver IV] Title: 대칭 차집합, Time: 308 ms, Memory: 103712 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -0,0 +1,7 @@
+a, b = map(int,input().split())
+aset = set(map(int,input().split()))
+bset = set(map(int,input().split()))
+rset = (aset | bset) - (aset & bset)
+rset = list(rset)
+print(len(rset))
+#암만 생각해도 파이썬 개날먹임 ㄹㅇ
